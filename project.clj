ea2ba6ad13bf5de4d352796240a266906167e1c6
August Lilleaas <august@augustl.com>
2018-10-17 13:12:41 +0200

    Update all dependencies (no code changes needed)

--- project.clj
+++ project.clj
@@ -5,16 +5,16 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [stasis "2.3.0"]
-                 [ring "1.2.1"]
-                 [ring/ring-jetty-adapter "1.2.1"]
-                 [joda-time "2.3"]
+                 [ring "1.7.0"]
+                 [ring/ring-jetty-adapter "1.7.0"]
+                 [joda-time "2.10"]
                  [hiccup "1.0.5"]
                  [optimus "0.14.2"]
-                 [org.clojure/tools.nrepl "0.2.2"]
+                 [org.clojure/tools.nrepl "0.2.13"]
                  [augusts-fancy-blog-post-parser "0.1.0"]
-                 [com.stuartsierra/component "0.2.3"]
+                 [com.stuartsierra/component "0.3.2"]
                  [org.pegdown/pegdown "1.6.0"]]
   :aliases {"export" ["run" "-m" "augustl-com.cli/export" "dist"]}
   :profiles
   {:dev {:source-paths ["dev"]
-         :dependencies [[reloaded.repl "0.1.0"]]}})
+         :dependencies [[reloaded.repl "0.2.4"]]}})
