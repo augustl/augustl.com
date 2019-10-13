89bd4519bd2c1f4555a6e5e1fb09f9f55c9eaf08
August Lilleaas <august@augustl.com>
2019-10-13 22:52:44 +0200

    Add cloudfront invalidation to deploy scropt

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 lein export
 cp resources/keybase.txt dist/keybase.txt
 aws s3 sync dist/ s3://augustl-com-static-web/ --delete
+
+# aws cloudfront create-invalidation --distribution-id E1OOI5QPC4QPU --paths "/*"
