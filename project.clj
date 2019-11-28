151564e894b4d04cd5b68354d807ff7e16eb6400
August Lilleaas <august@augustl.com>
2019-11-28 14:10:51 +0100

    Bump deps - works on windows yay

--- project.clj
+++ project.clj
@@ -9,10 +9,9 @@
                  [ring/ring-jetty-adapter "1.7.0"]
                  [joda-time "2.10"]
                  [hiccup "1.0.5"]
-                 [optimus "0.20.1"]
+                 [optimus "1.0.0-rc2"]
                  [org.clojure/tools.nrepl "0.2.13"]
-                 [augusts-fancy-blog-post-parser "0.1.0"]
-                 [clygments "2.0.0"]
+                 [augusts-fancy-blog-post-parser "0.2.0"]
                  [com.stuartsierra/component "0.3.2"]
                  [com.vladsch.flexmark/flexmark-all "0.50.42"]]
   :aliases {"export" ["run" "-m" "augustl-com.cli/export" "dist"]}
