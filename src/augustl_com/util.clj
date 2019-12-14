041da3409081fe56aec0ee5c977760c2955c2792
August Lilleaas <august@augustl.com>
2019-12-14 20:54:02 +0100

    support markdown tables

--- src/augustl_com/util.clj
+++ src/augustl_com/util.clj
@@ -1,12 +1,17 @@
 (ns augustl-com.util
   (:import (com.vladsch.flexmark.html HtmlRenderer)
-           (com.vladsch.flexmark.parser Parser)))
+           (com.vladsch.flexmark.parser Parser)
+           (com.vladsch.flexmark.ext.tables TablesExtension)
+           (com.vladsch.flexmark.util.data MutableDataSet)))
 
 (defmulti get-post-body :extension)
 
+(def opts (-> (MutableDataSet.)
+              (.set Parser/EXTENSIONS [(TablesExtension/create)])))
+
 (defmethod get-post-body "md" [post]
-  (let [parser (.build (Parser/builder))
-        renderer (.build (HtmlRenderer/builder))
+  (let [parser (.build (Parser/builder opts))
+        renderer (.build (HtmlRenderer/builder opts))
         node (.parse parser ((:get-body post)))]
     (.render renderer node)))
 
