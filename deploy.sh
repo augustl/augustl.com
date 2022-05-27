6e473a85e93ad38df1ecf5ed49ab1f6af8e39a6e
August Lilleaas <august@augustl.com>
2022-05-27 14:53:58 +0200

    I no longer use keybase

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 lein export
-cp resources/keybase.txt dist/keybase.txt
 aws s3 sync dist/ s3://augustl-com-static-web/ --delete
 
 # aws cloudfront create-invalidation --distribution-id E1OOI5QPC4QPU --paths "/*"
